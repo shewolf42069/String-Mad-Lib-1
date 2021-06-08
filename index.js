03eb777c3ab33debeef6d0f583026fe571c2204d
LoriPoje <>
2021-06-08 04:36:45 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let pluralNoun = "ties";
+let name = "Lori";
+let verb = "run";
+let adjective = "crispy";
+let color = "brown";
+
+console.log(`JavaScript provides a ${color} collection of tools — including ${adjective} syntax and ${pluralNoun} — that allows ${name} to ${verb} with strings.`);
